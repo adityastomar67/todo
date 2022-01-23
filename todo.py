5802e936bbd169099d77657ae07c3c54766a2a02
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-01-23 14:54:43 +0530

    Changed all the HardCodes

--- todo.py
+++ todo.py
@@ -2,6 +2,9 @@ import typer
 from rich.console import Console
 from rich.table import Table
 
+from database import complete_todo, delete_todo, get_all_todos, insert_todo, update_todo
+from model import Todo
+
 console = Console()
 app = typer.Typer()
 
@@ -9,34 +12,35 @@ app = typer.Typer()
 @app.command(short_help="Adds the Task to the list")
 def add(task: str, category: str):
     typer.echo(f"Adding task: {task} in category: {category}")
+    todo = Todo(task, category)
+    insert_todo(todo)
     show()
 
 
 @app.command(short_help="Removes the Task from the list")
 def remove(position: int):
     typer.echo(f"Removing task: {position}")
+    delete_todo(position - 1)
     show()
 
 
 @app.command(short_help="Updates the task in the list")
 def update(position: int, task: str, category: str):
     typer.echo(f"Updating task: {position} to {task} in category: {category}")
+    update_todo(position - 1, task, category)
     show()
 
 
 @app.command()
 def complete(position: int):
     typer.echo(f"Marking completing task: {position} as completed")
+    complete_todo(position - 1)
     show()
 
 
 @app.command()
 def show():
-    tasks = [
-        ("Task 1", "Category 1"),
-        ("Task 2", "Category 2"),
-        ("Task 3", "Category 3"),
-    ]
+    tasks = get_all_todos()
 
     console.print("[bold magenta]Showing all tasks[/bold magenta]")
     table = Table(show_header=True, header_style="bold blue")
@@ -52,9 +56,11 @@ def show():
         return "white"
 
     for idx, task in enumerate(tasks, start=1):
-        clr = get_category_color(task[1])
-        is_done_str = "✅" if True == 2 else "❌"
-        table.add_row(str(idx), task[0], f"[{clr}]{task[1]}[/{clr}]", is_done_str)
+        clr = get_category_color(task.category)
+        is_done_str = "✅" if task.status == 2 else "❌"
+        table.add_row(
+            str(idx), task.task, f"[{clr}]{task.category}[/{clr}]", is_done_str
+        )
     console.print(table)
 
 
